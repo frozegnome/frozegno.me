80e36e2ec12ae4eae7da583c8b05eef923abc54b
pcaron11 <pcaron@att.net>
2015-12-07 15:30:43 -0500

    Basic angular scaffolding

--- dist/scripts/main.js
+++
@@ -1,1 +0,0 @@
-!function(e){"use strict";e("img[usemap]").rwdImageMaps()}(jQuery);
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('frozegnomeGithubIo', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router']);
+
+})();
